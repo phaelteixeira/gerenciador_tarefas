5d71be190c2cea325335e446f7c3ae5d6b3251b7
phaelteixeira <45774900+phaelteixeira@users.noreply.github.com>
2019-05-19 16:46:05 -0300

    alterado o caminho dos botoes tarefas e tipo de tarefas

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,8 +13,8 @@
         <div class="sidebar-heading"><h3>Gerenciador de tarefas</h3> </div>
         <div class="list-group list-group-flush">
             <a href="usuario_cadastrar" class="list-group-item list-group-item-action bg-light">Usuario</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Tarefas</a>
-            <a href="#" class="list-group-item list-group-item-action bg-light">Tipo de tarefas</a>
+            <a href="tarefa_cadastrar" class="list-group-item list-group-item-action bg-light">Tarefas</a>
+            <a href="tipotarefa_cadastrar" class="list-group-item list-group-item-action bg-light">Tipo de tarefas</a>
         </div>
         </div>
         <!-- /#sidebar-wrapper -->
